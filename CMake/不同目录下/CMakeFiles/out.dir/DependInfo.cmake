83402aa9c78207e29cc8f2bc99c9785d79e35db8
sunrose050427 <630683723@qq.com>
2024-01-22 16:57:36 +0800

    实现了在同一目录下与不同目录下的多文件编程

---
+++ CMake/不同目录下/CMakeFiles/out.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Git/FileTxt/mainCode/Stack.c" "CMakeFiles/out.dir/mainCode/Stack.c.obj" "gcc" "CMakeFiles/out.dir/mainCode/Stack.c.obj.d"
+  "D:/Git/FileTxt/mainCode/mian.c" "CMakeFiles/out.dir/mainCode/mian.c.obj" "gcc" "CMakeFiles/out.dir/mainCode/mian.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
